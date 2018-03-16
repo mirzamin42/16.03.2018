ba4750e44c7db25325743b1abce5a38f5a950a70
mirzamin42 <mirzaminrs@code.edu.az>
2018-03-16 08:07:36 +0400

    Suallarin cavabi

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1ci sualin cavabi---parametri metoda oturmeyin 3 ylu var: const fields, Constructor vasitesiynen, asagidaki usul da var, ancaq Meliim, ele bu Constructor deyilki?
+            var obj = new StaticsOfEarth();
+            obj.DetailsOfEarth();
+            //2ci usul
+            var bjo = new EvinSahesi(25, 54);
+            bjo.Sahe();
+
+            Futbolcu xususiyyetler = new Futbolcu("Leonel", "Messi", 29);
+            
+
+
+        }
+    }
+     
+    public class StaticsOfEarth
+    {
+        //usul 1 ||
+        public const float HerketSureti = 9.8f;
+        public const float AtmosferTezyiqi = 760.0127f;
+
+        public void DetailsOfEarth()
+        {
+            Console.WriteLine("Yerin gunesh etrafinda firlanma sureti {0} m/s, ve atmosfer tezyiqi {1} mm cive stunudur", HerketSureti, AtmosferTezyiqi);
+        }
+    }
+    public class EvinSahesi
+    {
+        //usul 2 ||  
+        public readonly int En = 25;
+        public readonly int Uzunlug = 54;
+
+       public EvinSahesi(int en, int uzunlug)
+        {
+            En = en; Uzunlug= uzunlug;
+        }
+       public void Sahe()
+        {
+            Console.WriteLine("Bizim Evin Sahesi : "+(En*Uzunlug));
+        }
+    }
+    public class Futbolcu
+    {
+        //usul 3 ||
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Futbolcu (string name, string surname, int age)
+        {
+            name = name;
+            surname = surname;
+            age = age;
+        }
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public int agr
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+    }
+}
---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 11ci sualin cavabi:
+             * intheranc miras demekdi, bunu c# da isi daha asan yerine yetirmek ucun isdifade edirler,
+             * meselen lamorgini masinini yaradanda her ibyekt ucun her defe ayri bi sinif yaradib yeniden onu isdifade etmek evezine
+             * 1 defe sinif yaradib teze yaranan sinife miras buraxiriq belece her defe hedden artiq kod yazmaga ehtiyyac qalmir,
+             * hedden artiq yazilan kodu idare etmek de cetindi cinki gelecekde hansisa xirda bir detali deyisende gerekn butun yaranmis siniflerdeki 
+             * propertileri, methodlari deyishesen, bunun evezine intherance yolu ile miras goturulen sinifde deyisiklik etmekle problemi daha asan 
+             * hell elemek mumkun olur.*/
+
+            /* 12ci sualin cavabi:
+             * C# bir nece miras usulunu destekliyir, ancaq her defe yeni yaranan sinif ozunden 1 defe evvel yaranmis sinifi miras almaqla
+             * bu ish heyata kecirilir, ola biler ki yeni yaranan bir sinif ozunde 1 evvel yaranmis sinifi yox 2 3 nesil evvel yaranmis sinifi bir basa 
+             * miras alsin. Evvel yaranmis sinif yeni yaranmis sinifi miras ala bilmez o yalniz miras oturule biler. */
+
+             
+
+
+        }
+        class Baba
+        {
+
+        }
+        class Ata:Baba
+        {
+
+        }
+        class Neve : Baba
+        {
+
+        }
+        //class Neve : Ata
+        //{
+
+        //} 
+                    /*13cu sualin cavabi: C# da her yaranan yeni sinif ozunden evvel yaranmis sinifi bu yolla miras alir:  */
+
+    }
+    
+
+
+
+
+    
+}
